90e4301ceeae48ef536202913a0c4074858bd86a
miladfah <milad.fahimi@hiq.se>
2020-08-12 19:46:12 +0200

    Fix some style for comments

--- wp-content/themes/toomoney/comments.php
+++ wp-content/themes/toomoney/comments.php
@@ -3,17 +3,13 @@
     function format_comment($comment, $args, $depth) {
     $GLOBALS['comment'] = $comment; ?>
 <div class="row">
-    <div class="col-lg-2 col-md-2 col-sm-3 col-xs-12">
-        <div class="full">
-            <div class="img-responsive img-circle" style="max-width:100px">
-                <?php echo get_avatar(get_user_by("user_email", get_comment_author_email($comment_ID)),100)?>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-10 col-md-10 col-sm-9 col-xs-12">
+
+    <div class="col-lg-12 col-md-12 col-sm-10 col-xs-12">
         <?php if(get_comment_author($comment_ID)==get_the_author()) { 
             ?>
         <div class="full command_cont" style="background: #e9d16f;">
+            <?php echo get_avatar(get_comment_author_email($comment_ID),40)?>
+
             <p class="comm_head"><?php comment_author($comment_ID);?>
                 <span><?php comment_date($comment_ID)?></span>
                 <?php if ($comment->comment_approved == '0') : ?>
@@ -31,13 +27,19 @@
         <?php }else{ ?>
         <div class="row">
             <div class="col-lg-10 col-md-10 col-sm-9 col-xs-12">
-                <div class="full command_cont margin_bottom_0">
+                <div class="full command_cont command_cont_color margin_bottom_0">
+                    <div>
+                        <?php echo get_avatar(get_comment_author_email($comment_ID),40)?>
+                    </div>
                     <p class="comm_head"><?php comment_author($comment_ID)?>
                         <span><?php comment_date($comment_ID)?></span><?php if ($comment->comment_approved == '0') : ?>
                         <em>
                             <php _e('Your comment is awaiting moderation.') ?>
                         </em><br />
                         <?php endif; ?>
+                        <div class="rply">
+                            <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+                        </div>
                     </p>
                     <p><?php comment_text(); ?>
                     </p>
