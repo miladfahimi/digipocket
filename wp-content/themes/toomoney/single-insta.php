e25fd2aa4e037c407b6ecf169e14ad63e5937f7f
miladfah <milad.fahimi@hiq.se>
2020-09-17 20:36:43 +0200

    Fix title in minibar for insta template

--- wp-content/themes/toomoney/single-insta.php
+++ wp-content/themes/toomoney/single-insta.php
@@ -1,9 +1,25 @@
 <?php /* Template Name: Instagram */ 
  get_header(); ?>
 
-<?php
-    get_template_part('template-part/content','miniheader');
-?>
+<section id="inner_page_infor" class="innerpage_banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="full">
+                    <div class="inner_page_info">
+                        <h3 style="text-transform:capitalize;"> محتوای شبکه های اجتماعی<h3>
+                                <ul>
+                                    <li><a href="<?php echo get_post_type_archive_link(print_r(get_post_type()));?>"
+                                            style="text-transform:capitalize;">اینستاگرام و فیسبوک</a></li>
+                                    <li><i class="fa fa-angle-right"></i></li>
+                                    <li><a href="#"></a></li>
+                                </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <!-- section -->
 <?php get_template_part( 'template-part/content', 'index' ); ?>
