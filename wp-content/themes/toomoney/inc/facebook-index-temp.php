4cc9250dbd6bd2406d5adbeca39e355f4af599f8
miladfah <milad.fahimi@hiq.se>
2020-09-26 17:02:44 +0200

    Fix Facebook template

--- wp-content/themes/toomoney/inc/facebook-index-temp.php
+++ wp-content/themes/toomoney/inc/facebook-index-temp.php
@@ -1,13 +1,13 @@
 <?php
 function convertToFacebookIndexTemplate($priceSek,$priceNok,$priceDkk,$date){
-$html = ' <div class="cont-facebook" style="background-image:linear-gradient(to right top,rgba(27, 51, 77, 0.6),rgba(27, 51, 77, 0.8)),url(https://digipocket.ir/wp-content/themes/toomoney/images/slider_img1.png)">
+$html = '<div class="cont-facebook" style="background-image:linear-gradient(to right top,rgba(27, 51, 77, 0.6),rgba(27, 51, 77, 0.8))">
 <ul class="rows-facebook">
-    <li class="toomoney-logo">
+    <li class="toomoney_logo_fs">
         <img src="https://digipocket.ir/wp-content/themes/toomoney/images/logos/logo_2.png" >
     </li>
 
     <li class="columns">
-        <div class="cells">
+        <div class="cells-s">
             <img src="https://digipocket.ir/wp-content/themes/toomoney/images/sweden.jpg">
             <span>'.$priceSek.'</span>
             <p>تومان</p>
@@ -20,12 +20,12 @@ $html = ' <div class="cont-facebook" style="background-image:linear-gradient(to 
     </li>
 
     <li class="columns">
-        <div class="cells">
+        <div class="cells-s">
             <img src="https://digipocket.ir/wp-content/themes/toomoney/images/norwegin.jpg">
             <span>'.$priceNok.'</span>
             <p>تومان</p>
         </div>
-        <div class="cells">
+        <div class="cells-s">
             <img src="https://digipocket.ir/wp-content/themes/toomoney/images/etherium.png">
             <span style="font-size:17px">11397</span>
             <p>دلار</p>
@@ -33,12 +33,12 @@ $html = ' <div class="cont-facebook" style="background-image:linear-gradient(to 
     </li>
 
     <li class="columns">
-        <div class="cells">
+        <div class="cells-s">
             <img src="https://digipocket.ir/wp-content/themes/toomoney/images/denmark.jpg">
             <span>'.$priceDkk.'</span>
             <p>تومان</p>
         </div>
-        <div class="cells">
+        <div class="cells-s">
             <img src="https://digipocket.ir/wp-content/themes/toomoney/images/ripple.png">
             <span style="font-size:17px">11397</span>
             <p>دلار</p>
@@ -52,115 +52,102 @@ $html = ' <div class="cont-facebook" style="background-image:linear-gradient(to 
 </div>';
 
 $css = '@font-face {
-font-family: "vazir";
-src: url("https://digipocket.ir/wp-content/themes/toomoney/fonts/Vazir-FD-WOL.eot");
-src: url("https://digipocket.ir/wp-content/themes/toomoney/fonts/Vazir-FD-WOL.eot#iefix") format("embedded-opentype"),
-url("fonts/Vazir-FD-WOL.woff") format("woff"),
-url("https://digipocket.ir/wp-content/themes/toomoney/fonts/Vazir-FD-WOL.woff2") format("woff2"),
-url("https://digipocket.ir/wp-content/themes/toomoney/fonts/Vazir-FD-WOL.ttf") format("truetype"),
-url("https://digipocket.ir/wp-content/themes/toomoney/fonts/Vazir-FD-WOL.svg#CartoGothicStdBook") format("svg");
-font-weight: normal;
-font-style: normal;
-}
-.templates-cntr {
-display: flex;
-margin-top: 70px;
-}
-@media (max-width: 500px) {
-.templates-cntr {
-flex-direction: column;
-margin: 30px;
-justify-content: space-between;
-height: 700px;
-}
-}
-
-.cont-facebook {
-position: relative;
-overflow: hidden;
-direction: rtl;
-font-family: "vazir";
-width: 300px;
-height: 156px;
-color: #e9d16f;
-background-size: cover;
-background-position: right;
-}
-
-.rows-facebook {
-display: flex;
-margin-top: 30px;
-flex-direction: column;
-align-items: center;
-list-style-type: none;
-padding: 10px;
-}
-.columns {
-display: flex;
-justify-content: center;
-}
-
-.cells-s {
-display: flex;
-height: 20px;
-width: 120px;
-margin: 2px;
-text-align: center;
-border: 1px solid #e9d16f;
-border-radius: 3px;
-}
-
-
-}
-.cells-content-img {
-justify-content: center;
-height: 120px;
-width: 250px;
-margin-top: 5px;
-text-align: center;
-font-size: 10px;
-color: #1b334d;
-border-radius: 5px;
-}
-
-.cells-full-sub {
-height: 20px;
-width: 250px;
-margin: 5px;
-text-align: center;
-font-size: 8px;
-color: #e9d16f;
-}
-
-.cells-s img {
-flex: 35% 0 1;
-height: 100%;
-width: 35%;
-object-fit: cover;
-border-top-right-radius: 4px;
-border-bottom-right-radius: 4px;
-}
-
-.cells-s span {
-font-size: 14px;
-padding: 0 8px;
-color: #ffe78f;
-padding-left: 0;
-}
-.cells-s p {
-font-size: 8px;
-color: #e9d16f;
-}
-
-.toomoney-logo-facebook {
-position: absolute;
-top: 2px;
-width: 70px;
-}
-.toomoney-logo-facebook img {
-height: 100%;
-width: 100%;
-}';
+    font-family: "vazir";
+    src: url("https://digipocket.ir/wp-content/themes/toomoney/fonts/Vazir-FD-WOL.eot");
+    src: url("https://digipocket.ir/wp-content/themes/toomoney/fonts/Vazir-FD-WOL.eot#iefix") format("embedded-opentype"),
+    url("fonts/Vazir-FD-WOL.woff") format("woff"),
+    url("https://digipocket.ir/wp-content/themes/toomoney/fonts/Vazir-FD-WOL.woff2") format("woff2"),
+    url("https://digipocket.ir/wp-content/themes/toomoney/fonts/Vazir-FD-WOL.ttf") format("truetype"),
+    url("https://digipocket.ir/wp-content/themes/toomoney/fonts/Vazir-FD-WOL.svg#CartoGothicStdBook") format("svg");
+    font-weight: normal;
+    font-style: normal;
+    }
+    .templates-cntr {
+        display: flex;
+        margin-top: 70px;
+    }
+    @media (max-width: 500px) {
+        .templates-cntr {
+            flex-direction: column;
+            margin: 30px;
+            justify-content: space-between;
+            height: 700px;
+        }
+    }
+    .cont-facebook {
+        position: relative;
+        overflow: hidden;
+        direction: rtl;
+        font-family: "vazir";
+        width: 300px;
+        height: 156px;
+        color: #e9d16f;
+        background-size: cover;
+        background-position: right;
+    }
+    
+    .rows-facebook {
+        display: flex;
+        margin-top: 30px;
+        flex-direction: column;
+        align-items: center;
+        list-style-type: none;
+        padding: 10px;
+    }
+    .columns {
+        display: flex;
+        justify-content: center;
+    }
+    
+    .cells-s {
+        display: flex;
+        height: 20px;
+        width: 120px;
+        margin: 2px;
+        text-align: center;
+        border: 1px solid #e9d16f;
+        border-radius: 3px;
+    }
+    
+    .cells-full-sub {
+        height: 20px;
+        width: 250px;
+        margin: 5px;
+        text-align: center;
+        font-size: 8px;
+        color: #e9d16f;
+    }
+    
+    .cells-s img {
+        flex: 35% 0 1;
+        height: 100%;
+        width: 35%;
+        object-fit: cover;
+        border-top-right-radius: 4px;
+        border-bottom-right-radius: 4px;
+    }
+    
+    .cells-s span {
+        font-size: 14px;
+        padding: 0 8px;
+        color: #ffe78f;
+        padding-left: 0;
+    }
+    .cells-s p {
+        font-size: 8px;
+        color: #e9d16f;
+    }
+    
+    .toomoney_logo_fs {
+      position: absolute;
+      top: 5px;
+      height: 30px;
+      width: 60px;
+    }
+    .toomoney_logo_fs img {
+      height: 100%;
+      width: 100%;
+    }';
 
 $client = new \GuzzleHttp\Client();
 // Retrieve your user_id and api_key from https://htmlcsstoimage.com/dashboard
